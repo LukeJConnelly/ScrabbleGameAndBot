94fe5c10ef253e7ab289e82661c1db23bc85f867
SeanJevens <48124240+SeanJevens@users.noreply.github.com>
2020-02-21 10:52:32 +0000

    placement of first word & tests

--- Board.java
+++ Board.java
@@ -1,171 +1,181 @@
 import java.util.ArrayList;
 
 public class Board {
-	private int[][] boardValues = {
-		{4,0,0,1,0,0,0,4,0,0,0,1,0,0,4},
-		{0,3,0,0,0,2,0,0,0,2,0,0,0,3,0},
-		{0,0,3,0,0,0,1,0,1,0,0,0,3,0,0},
-		{1,0,0,3,0,0,0,1,0,0,0,3,0,0,1},
-		{0,0,0,0,3,0,0,0,0,0,3,0,0,0,0},
-		{0,2,0,0,0,2,0,0,0,2,0,0,0,2,0},
-		{0,0,1,0,0,0,1,0,1,0,0,0,1,0,0},
-		{4,0,0,1,0,0,0,3,0,0,0,1,0,0,4},
-		{0,0,1,0,0,0,1,0,1,0,0,0,1,0,0},
-		{0,2,0,0,0,2,0,0,0,2,0,0,0,2,0},
-		{0,0,0,0,3,0,0,0,0,0,3,0,0,0,0},
-		{1,0,0,3,0,0,0,1,0,0,0,3,0,0,1},
-		{0,0,3,0,0,0,1,0,1,0,0,0,3,0,0},
-		{0,3,0,0,0,2,0,0,0,2,0,0,0,3,0},
-		{4,0,0,1,0,0,0,4,0,0,0,1,0,0,4}
-	};
-	private char[][] boardTiles = new char[15][15];
-	public void boardReset()
-	{
-		int i,j;
-		for (i=0;i<15;i++)
-		{
-			for (j=0;j<15;j++)
-			{
-				boardTiles[i][j]='\0';
-			}
-		}
-	}
-	 public String toString()
-	    {
-	        String s="";
+    private int[][] boardValues = {
+            {4, 0, 0, 1, 0, 0, 0, 4, 0, 0, 0, 1, 0, 0, 4},
+            {0, 3, 0, 0, 0, 2, 0, 0, 0, 2, 0, 0, 0, 3, 0},
+            {0, 0, 3, 0, 0, 0, 1, 0, 1, 0, 0, 0, 3, 0, 0},
+            {1, 0, 0, 3, 0, 0, 0, 1, 0, 0, 0, 3, 0, 0, 1},
+            {0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0},
+            {0, 2, 0, 0, 0, 2, 0, 0, 0, 2, 0, 0, 0, 2, 0},
+            {0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0},
+            {4, 0, 0, 1, 0, 0, 0, 3, 0, 0, 0, 1, 0, 0, 4},
+            {0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0},
+            {0, 2, 0, 0, 0, 2, 0, 0, 0, 2, 0, 0, 0, 2, 0},
+            {0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0},
+            {1, 0, 0, 3, 0, 0, 0, 1, 0, 0, 0, 3, 0, 0, 1},
+            {0, 0, 3, 0, 0, 0, 1, 0, 1, 0, 0, 0, 3, 0, 0},
+            {0, 3, 0, 0, 0, 2, 0, 0, 0, 2, 0, 0, 0, 3, 0},
+            {4, 0, 0, 1, 0, 0, 0, 4, 0, 0, 0, 1, 0, 0, 4}
+    };
+    private char[][] boardTiles = new char[15][15];
 
-	        //pritning of column letters
-	        System.out.println("  A   B   C   D   E   F   G   H   I   J   K   L   M   N   O");
+    public void boardReset() {
+        int i, j;
+        for (i = 0; i < 15; i++) {
+            for (j = 0; j < 15; j++) {
+                boardTiles[i][j] = '\0';
+            }
+        }
+    }
 
-	        s+="| ----------------------------------------------------------- |\n";
+    public String toString() {
+        String s = "";
 
-	        int i,j;
-	        for (i=0;i<15;i++)
-	        {
-	            for(j=0;j<15;j++)
-	            {
+        //pritning of column letters
+        System.out.println("  A   B   C   D   E   F   G   H   I   J   K   L   M   N   O");
 
-	                s+="| ";
-	                if (boardTiles[i][j]!='\0')
-	                {
-	                    s+=boardTiles[i][j] + " ";
-	                }
-	                else
-	                {
-	                    switch(this.boardValues[i][j])
-	                    {
-	                        case 0:
-	                        {
-	                            s+="_ ";
-	                            break;
-	                        }
-	                        case 1:
-	                        {
-	                            s+="d ";
-	                            break;
-	                        }
-	                        case 2:
-	                        {
-	                            s+="t ";
-	                            break;
-	                        }
-	                        case 3:
-	                        {
-	                            s+="2 ";
-	                            break;
-	                        }
-	                        case 4:
-	                        {
-	                            s+="3 ";
-	                            break;
-	                        }
-	                    }
-	                }
-	            }
+        s += "| ----------------------------------------------------------- |\n";
 
-	            s+= "| | " +(i+1);  //prints the row number
+        int i, j;
+        for (i = 0; i < 15; i++) {
+            for (j = 0; j < 15; j++) {
 
-	            s+="\n| ----------------------------------------------------------- |\n";
-	        }
-	        return s;
-	    }
-	public void addTile(char c, int x, int y)
-	{
-		boardTiles[y][x]=c;
-	}
-	public void removeTile(int x, int y)
-	{
-		boardTiles[y][x]='\0';
-	}
-	public boolean containsTile(int x, int y)
-	{
-		return !(boardTiles[y][x]=='\0');
-	}
-	public char getBoardTile(int x, int y)
-	{
-		return this.boardTiles[y][x];
-	}
-	public boolean legalPlacement(int row, int col, String word, boolean direction, Frame myFrame) { //not first word placed	// bool direction 1=right, 0=down
-		boolean flagEmptySquare = false, flagFullSquare = false; //1 - touch at least one empty square and 2 - touch at least one full square
-		int k;
-		int right = direction ? 1 : 0;
-		int down = !direction ? 1 : 0;
-		ArrayList<Character> arrWord = convertWordToArrayList(word);
-		ArrayList<Character> charsToBeReturned = new ArrayList<Character>();
-		if (row < 0 || row + (right * word.length()) > 14 || col < 0 || col + (down * word.length()) > 14)
-		{
-			return false;
-		}
-		for (k = 0; k < arrWord.size(); k++) {
-			if (!containsTile(row + k * right, col + k * down)) {
-				if(myFrame.checkLetter(arrWord.get(k))) {
-					myFrame.removeLetter(arrWord.get(k));
-					charsToBeReturned.add(arrWord.get(k));
-					flagEmptySquare = true;
-				}
-				else
-				{
-					myFrame.letters.addAll(charsToBeReturned);
-					return false;
-				}
-			}
-			else {
-				flagFullSquare = true;
-				if(getBoardTile(row + k * right, col + k * down)!=arrWord.get(k))
-				{
-					myFrame.letters.addAll(charsToBeReturned);
-					return false;
-				}
-			}
-		}
-		myFrame.letters.addAll(charsToBeReturned);
-		if(!(flagEmptySquare&&flagFullSquare))
-		{
-			return false;
-		}
-		else
-		{
-			return true;
-		}
-	}
-	public ArrayList<Character> convertWordToArrayList(String word){
-		ArrayList<Character> brokenWord = new ArrayList<Character>();
-		for(char c : word.toCharArray()){
-			brokenWord.add(c);
-		}
-		return brokenWord;
-	}
-	public void placeWord(int wantedRow, int wantedCol, String wantedWord, boolean direction, Frame myFrame)
-	{
-		int k;
-		int right = direction ? 1 : 0;
-		int down = !direction ? 1 : 0;
-		ArrayList<Character> arrWord = convertWordToArrayList(wantedWord);
-		for (k = 0; k < arrWord.size(); k++) {
-			if (!containsTile(wantedRow + k * right, wantedCol + k * down)) {
-				myFrame.removeLetter(arrWord.get(k));
-				addTile(arrWord.get(k), wantedRow + k * right, wantedCol + k * down);
-			}
-		}
-	}
+                s += "| ";
+                if (boardTiles[i][j] != '\0') {
+                    s += boardTiles[i][j] + " ";
+                } else {
+                    switch (this.boardValues[i][j]) {
+                        case 0: {
+                            s += "_ ";
+                            break;
+                        }
+                        case 1: {
+                            s += "d ";
+                            break;
+                        }
+                        case 2: {
+                            s += "t ";
+                            break;
+                        }
+                        case 3: {
+                            s += "2 ";
+                            break;
+                        }
+                        case 4: {
+                            s += "3 ";
+                            break;
+                        }
+                    }
+                }
+            }
+
+            s += "| | " + (i + 1);  //prints the row number
+
+            s += "\n| ----------------------------------------------------------- |\n";
+        }
+        return s;
+    }
+
+    public void addTile(char c, int x, int y) {
+        boardTiles[y][x] = c;
+    }
+
+    public void removeTile(int x, int y) {
+        boardTiles[y][x] = '\0';
+    }
+
+    public boolean containsTile(int x, int y) {
+        return !(boardTiles[y][x] == '\0');
+    }
+
+    public char getBoardTile(int x, int y) {
+        return this.boardTiles[y][x];
+    }
+
+    public boolean legalPlacement(int row, int col, String word, boolean direction, Frame myFrame) {
+        //not first word placed	// bool direction 1=right, 0=down
+        boolean flagEmptySquare = false, flagFullSquare = false;
+        //1 - touch at least one empty square and 2 - touch at least one full square
+        int k;
+        int right = direction ? 1 : 0;
+        int down = !direction ? 1 : 0;
+        ArrayList<Character> arrWord = convertWordToArrayList(word);
+        ArrayList<Character> charsToBeReturned = new ArrayList<Character>();
+        if (row < 0 || row + (right * word.length()) > 14 || col < 0 || col + (down * word.length()) > 14) {
+            return false;
+        }
+        for (k = 0; k < arrWord.size(); k++) {
+            if (!containsTile(row + k * right, col + k * down)) {
+                if (myFrame.checkLetter(arrWord.get(k))) {
+                    myFrame.removeLetter(arrWord.get(k));
+                    charsToBeReturned.add(arrWord.get(k));
+                    flagEmptySquare = true;
+                } else {
+                    myFrame.letters.addAll(charsToBeReturned);
+                    return false;
+                }
+            } else {
+                flagFullSquare = true;
+                if (getBoardTile(row + k * right, col + k * down) != arrWord.get(k)) {
+                    myFrame.letters.addAll(charsToBeReturned);
+                    return false;
+                }
+            }
+        }
+        myFrame.letters.addAll(charsToBeReturned);
+        if (!(flagEmptySquare && flagFullSquare)) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public ArrayList<Character> convertWordToArrayList(String word) {
+        ArrayList<Character> brokenWord = new ArrayList<Character>();
+        for (char c : word.toCharArray()) {
+            brokenWord.add(c);
+        }
+        return brokenWord;
+    }
+
+    public void placeWord(int wantedRow, int wantedCol, String wantedWord, boolean direction, Frame myFrame) {
+        int k;
+        int right = direction ? 1 : 0;
+        int down = !direction ? 1 : 0;
+        ArrayList<Character> arrWord = convertWordToArrayList(wantedWord);
+        for (k = 0; k < arrWord.size(); k++) {
+            if (!containsTile(wantedRow + k * right, wantedCol + k * down)) {
+                myFrame.removeLetter(arrWord.get(k));
+                addTile(arrWord.get(k), wantedRow + k * right, wantedCol + k * down);
+            }
+        }
+    }
+
+
+    public boolean firstTurn( int x, int y, String word, boolean direction, Frame myFrame) {
+        //method to check the first player places a tile on the centre with first move
+
+        int k;
+        boolean flagCentre = false;
+        int right = direction ? 1 : 0;
+        int down = !direction ? 1 : 0;
+        ArrayList<Character> arrWord = convertWordToArrayList(word);
+        for (k = 0; k < arrWord.size(); k++) {
+            if (!myFrame.checkLetter(arrWord.get(k))) {
+                return false;
+            }
+            if ((x + k * right == 7) && (y + k * down == 7)) {
+                flagCentre = true;
+            }
+        }
+        return flagCentre;
+    }
+
 }
+
+
+
+
+
+
--- BoardTest.java
+++ BoardTest.java
@@ -1,68 +1,93 @@
 import java.util.ArrayList;
 
 public class BoardTest {
-	public static void main(String args[]){
-		Board myBoardTest = new Board();
-		System.out.println(myBoardTest.toString());
-		myBoardTest.addTile('T', 1, 2);
-		myBoardTest.addTile('E', 2, 2);
-		myBoardTest.addTile('S', 3, 2);
-		myBoardTest.addTile('T', 4, 2);
-		System.out.println(myBoardTest.toString());
-		System.out.println(myBoardTest.containsTile(4,2));
-		System.out.println(myBoardTest.containsTile(4,3));
-		myBoardTest.addTile('W', 4, 3);
-		myBoardTest.addTile('O', 4, 4);
-		System.out.println(myBoardTest.toString());
-		myBoardTest.removeTile(1, 2);
-		myBoardTest.removeTile(2, 2);
-		myBoardTest.removeTile(3, 2);
-		System.out.println(myBoardTest.toString());
-		myBoardTest.boardReset();
-		
-		// MOCK GAME
-		Frame myFrame = new Frame();
-		ArrayList<Character> chars = new ArrayList<>();
-		chars.add('O');
-		chars.add('O');
-		chars.add('L');
-		myFrame.getLetters().addAll(chars);
-		System.out.println(myBoardTest.toString());
-		myBoardTest.addTile('T', 7, 7);
-		myBoardTest.addTile('E', 8, 7);
-		myBoardTest.addTile('S', 9, 7);
-		myBoardTest.addTile('T', 10, 7);
-		System.out.println(myBoardTest.toString());
-		System.out.println(myBoardTest.getBoardTile(7,7)+" "+myBoardTest.getBoardTile(7,8));
-		System.out.println(myFrame.toString());
-		int wantedRow = 7, wantedCol = 7;
-		String wantedWord = "TOO";
-		boolean direction = false;
-		System.out.println(myBoardTest.legalPlacement(wantedCol, wantedRow, wantedWord, direction, myFrame));
-		myBoardTest.placeWord(wantedCol, wantedRow, wantedWord, direction, myFrame);
-		System.out.println(myBoardTest.toString());
-		System.out.println(myFrame.toString());
-		chars = new ArrayList<>();
-		chars.add('O');
-		chars.add('O');
-		chars.add('T');
-		chars.add('M');
-		myFrame.getLetters().addAll(chars);
-		wantedRow = 0;
-		wantedCol = 0;
-		direction = true;
-		System.out.println(myBoardTest.legalPlacement(wantedCol, wantedRow, wantedWord, direction, myFrame));
-		wantedRow = 7;
-		wantedCol = 10;
-		direction = false;
-		System.out.println(myBoardTest.legalPlacement(wantedCol, wantedRow, wantedWord, direction, myFrame));
-		myBoardTest.placeWord(wantedCol, wantedRow, wantedWord, direction, myFrame);
-		wantedRow = 6;
-		wantedCol = 8;
-		direction = false;
-		wantedWord="ME";
-		System.out.println(myBoardTest.legalPlacement(wantedCol, wantedRow, wantedWord, direction, myFrame));
-		myBoardTest.placeWord(wantedCol, wantedRow, wantedWord, direction, myFrame);
-		System.out.println(myBoardTest.toString());
-	}
+    public static void main(String args[]) {
+        Board myBoardTest = new Board();
+        System.out.println(myBoardTest.toString());
+
+
+        myBoardTest.addTile('T', 1, 2); //simple tests to add single tiles
+        myBoardTest.addTile('E', 2, 2);
+        myBoardTest.addTile('S', 3, 2);
+        myBoardTest.addTile('T', 4, 2);
+        System.out.println(myBoardTest.toString());
+        System.out.println(myBoardTest.containsTile(4, 2)); //tests of containsTile method
+        System.out.println(myBoardTest.containsTile(4, 3));
+        myBoardTest.addTile('W', 4, 3);
+        myBoardTest.addTile('O', 4, 4);
+        System.out.println(myBoardTest.toString());
+        myBoardTest.removeTile(1, 2);   //test of removeTile method
+        myBoardTest.removeTile(2, 2);
+        myBoardTest.removeTile(3, 2);
+        System.out.println(myBoardTest.toString());
+        myBoardTest.boardReset();   //test of boardReset method
+        System.out.println(myBoardTest.toString());
+
+
+
+
+// MOCK GAME
+        Frame myFrame = new Frame();
+        ArrayList<Character> chars = new ArrayList<>();
+        chars.add('T'); //adding letters to arraylist, then to frame
+        chars.add('E');
+        chars.add('S');
+        chars.add('T');
+        chars.add('O');
+        chars.add('O');
+        chars.add('L');
+        myFrame.getLetters().addAll(chars);
+        System.out.println(myBoardTest.toString());
+
+        int wantedRow = 9;
+        int wantedCol = 9;
+        String wantedWord = "TEST";
+        boolean direction = true;
+        System.out.println(myBoardTest.firstTurn(wantedCol, wantedRow, wantedWord, direction, myFrame));
+        //test for firstTurn trying to place first tiles off centre - should return false
+
+        wantedRow = 7;
+        wantedCol = 7;
+        wantedWord = "HELLO";   //second test for first turn with invalid word input- should return false
+        System.out.println(myBoardTest.firstTurn(wantedCol, wantedRow, wantedWord, direction, myFrame));
+
+        wantedWord = "TEST";    //final test for firstTurn- should return true
+        System.out.println(myBoardTest.firstTurn(wantedCol, wantedRow, wantedWord, direction, myFrame));
+        myBoardTest.placeWord(wantedCol, wantedRow, wantedWord, direction, myFrame);
+        System.out.println(myBoardTest.toString());
+
+        System.out.println(myBoardTest.getBoardTile(7, 7) + " " + myBoardTest.getBoardTile(7, 8));
+        System.out.println(myFrame.toString()); //printing of frame
+
+        wantedWord = "TOO";
+        System.out.println(myBoardTest.toString());
+        System.out.println(myFrame.toString());
+        chars = new ArrayList<>();
+        chars.add('O');
+        chars.add('O');
+        chars.add('T');
+        chars.add('M');
+        myFrame.getLetters().addAll(chars);
+        wantedRow = 0;
+        wantedCol = 0;
+        direction = true;
+        //tests for legalPlacement method- should be false
+        System.out.println(myBoardTest.legalPlacement(wantedCol, wantedRow, wantedWord, direction, myFrame));
+        wantedRow = 7;
+        wantedCol = 10;
+        direction = false;
+        //further test of legalPlacement- should be true
+        System.out.println(myBoardTest.legalPlacement(wantedCol, wantedRow, wantedWord, direction, myFrame));
+        myBoardTest.placeWord(wantedCol, wantedRow, wantedWord, direction, myFrame);
+
+        wantedRow = 6;
+        wantedCol = 8;
+        direction = false;
+        wantedWord = "ME";
+        //final test of legalPlacement - should be true
+        System.out.println(myBoardTest.legalPlacement(wantedCol, wantedRow, wantedWord, direction, myFrame));
+        myBoardTest.placeWord(wantedCol, wantedRow, wantedWord, direction, myFrame);
+        //placement of another word
+        System.out.println(myBoardTest.toString());
+    }
 }
