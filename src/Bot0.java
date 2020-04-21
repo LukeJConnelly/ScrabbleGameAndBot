e5bd47ff7228247e814ed0ab69c0947e416a61da
kkelly99 <karen.kelly1@ucdconnect.ie>
2020-04-21 13:39:28 +0100

    Work on shouldChallenge method

--- src/Bot0.java
+++ src/Bot0.java
@@ -100,7 +100,7 @@ public class Bot0 implements BotAPI {
         }
         if(maxScore != 0)
         {
-            command = Character.toString(bestWord.getFirstColumn()+'A') + Integer.toString(bestWord.getFirstRow()+1);
+            command = Character.toString((char) (bestWord.getFirstColumn()+'A')) + Integer.toString(bestWord.getFirstRow()+1);
             command += bestWord.isHorizontal() ? " A ":" D ";
             command += bestWord.toString(); //creates command for the best word
             command += blanks;
@@ -152,7 +152,7 @@ public class Bot0 implements BotAPI {
                     {
                         if(s.length()-s.replaceAll("_","").length()==2) {
                             for (int j=0; j<26;j++) {
-                                Word temp = new Word(sr, sc, g.isHorizontal, s, Character.toString((char) i+'A')+Character.toString((char) j+'A'));
+                                Word temp = new Word(sr, sc, g.isHorizontal, s, Character.toString((char) ((char) i+'A'))+Character.toString((char) ((char) j+'A')));
                                 ArrayList<Word> tempwords = new ArrayList<>();
                                 tempwords.add(temp);
                                 if(board.isLegalPlay(frame, temp)) {
@@ -169,7 +169,7 @@ public class Bot0 implements BotAPI {
                                             if (score > maxScore) {
                                                 bestWord = temp;
                                                 maxScore = score;
-                                                blanks = " "+Character.toString((char) i + 'A') + Character.toString((char) j + 'A');
+                                                blanks = " "+Character.toString((char) ((char) i + 'A')) + Character.toString((char) ((char) j + 'A'));
                                             }
                                         }
                                     }
@@ -177,7 +177,7 @@ public class Bot0 implements BotAPI {
                             }
                         }
                         else{
-                            Word temp = new Word(sr, sc, g.isHorizontal, s, Character.toString((char) i+'A'));
+                            Word temp = new Word(sr, sc, g.isHorizontal, s, Character.toString((char) ((char) i+'A')));
                             ArrayList<Word> tempwords = new ArrayList<>();
                             tempwords.add(temp);
                             if(board.isLegalPlay(frame, temp)) {
@@ -194,7 +194,7 @@ public class Bot0 implements BotAPI {
                                         if (score > maxScore) {
                                             bestWord = temp;
                                             maxScore = score;
-                                            blanks = " "+Character.toString((char) i + 'A');
+                                            blanks = " "+Character.toString((char) ((char) i + 'A'));
                                         }
                                     }
                                 }
@@ -230,7 +230,7 @@ public class Bot0 implements BotAPI {
         }
         if(maxScore != 0)
         {
-            command = Character.toString(bestWord.getFirstColumn()+'A') + Integer.toString(bestWord.getFirstRow()+1);
+            command = Character.toString((char) (bestWord.getFirstColumn()+'A')) + Integer.toString(bestWord.getFirstRow()+1);
             command += bestWord.isHorizontal() ? " A ":" D ";
             command += bestWord.toString(); //creates command for the best word
             command += blanks;
@@ -445,7 +445,7 @@ public class Bot0 implements BotAPI {
         for(int i=0;i<s.length();i++) {
             if (s.charAt(i) == '_') {
                 for (int j = 0; j < 26; j++) {
-                    s = s.substring(0, i) + Character.toString('A' + j) + s.substring(i + 1);
+                    s = s.substring(0, i) + Character.toString((char) ('A' + j)) + s.substring(i + 1);
                     if (s.substring(i + 1).contains("_")) {
                         addStringsWithoutBlanks(s, al);
                     } else {
